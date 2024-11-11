79bf2346ed6ba107be0ffce43a970bc11660d347
Eric <halakoei@yahoo.com>
2024-11-11 22:18:26 +0330

    models: update models

--- app/models.py
+++ app/models.py
@@ -18,8 +18,11 @@ class Category(db.Model):
 class NewsLink(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     url = db.Column(db.String(500), nullable=False)
-    news_id = db.Column(db.Integer, db.ForeignKey('news.id'), nullable=False)
-    news = db.relationship('News', back_populates='links')
+    news_id = db.Column(db.Integer, db.ForeignKey('news.id'), nullable=False, unique=True)
+    news = db.relationship('News', back_populates='link', uselist=False)
+
+    def __repr__(self):
+        return self.url
 
 class News(db.Model):
     id = db.Column(db.Integer, primary_key=True)
@@ -29,7 +32,7 @@ class News(db.Model):
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
     image_url = db.Column(db.String(500))
     categories = db.relationship('Category', secondary=news_categories, back_populates='news')
-    links = db.relationship('NewsLink', back_populates='news')
+    link = db.relationship('NewsLink', back_populates='news', uselist=False)
 
     def get_time_passed(self):
         return time_passed(self.created_at)
